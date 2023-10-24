c5061f65a96dd55aa439343818fc056851bdb3fa
Devin Smith <rekerzombani@gmail.com>
2023-10-23 22:17:25 -0500

    altered original drone Script

--- assets/scenes/drone_movement.gd
+++ assets/scenes/drone_movement.gd
@@ -9,7 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func _input(event):
-	if event.is_action_type("fly_up"):
-		
