39dcab8ae0ce0a665b0860ab3ab3436f4684c462
Parker Hix <parker.e.hix@gmail.com>
2023-10-26 00:29:25 -0500

    completed basic functionality for pick up code

--- assets/drop_zone/crate_detection.gd
+++ assets/drop_zone/crate_detection.gd
@@ -15,33 +15,25 @@ func _ready():
 	self.end = end_marker.global_position
 
 func _process(delta):
-	# curve path
+	# we must constantly update the end position because the drone is moving.
 	end = end_marker.global_position
 	if t <= 1 and colliding_body != null:
 		var curve_position : Vector2 = bezier_class.quadratic_bezier(start, middle, end, t)
 		colliding_body.position = curve_position
 		t += delta/0.465
 	else:
-		self.get_parent().set_deferred("freeze", false)
-		print(self.get_parent())
 		colliding_body.reparent(self.get_parent())
 		colliding_body.rotation = 0
-		colliding_body.position = Vector2()
-		# cleanup
-		set_process(false)
+		colliding_body.global_position = end_marker.global_position
+		colliding_body.set_deferred("frozen", true)
 
 func trigger_pickup():
 	var overlapping_bodies = self.get_overlapping_bodies()
 	for body in overlapping_bodies:
 		if not body is Antidote:
 			pass
-		self.get_parent().set_deferred("freeze", true)
 		self.colliding_body = body
-		body.freeze_mode = body.FREEZE_MODE_STATIC
-		body.set_deferred("freeze", true)
-		body.top_level = true
 		self.start = body.global_position
-		#colliding_body.global_position = self.start
 		var size = self.find_child("CollisionShape2D").shape.size
 		self.middle = self.global_position + Vector2(0, randf_range(-size.y/6, size.y/6))
 		self.set_process(true)
