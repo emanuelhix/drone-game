b4d8f304cc2e036e0d84787da7b5e440518e2338
Parker Hix <parker.e.hix@gmail.com>
2023-10-26 00:44:22 -0500

    fixed crate detection to be separate from pick-up detection

--- assets/drop_zone/crate_detection.gd
+++ assets/drop_zone/crate_detection.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var end_marker : Marker2D = find_child("End")
 @onready var bezier_class = preload("res://assets/globals/bezier.gd").new()
-signal item_picked_up(item : RigidBody2D)
+signal antidote_collected
 
 var t : float = 0
 var start : Vector2
@@ -11,39 +11,23 @@ var end : Vector2
 var colliding_body : RigidBody2D = null
 
 func _ready():
+	self.body_entered.connect(on_body_entered)
 	self.set_process(false)
 	self.end = end_marker.global_position
 
 func _process(delta):
-	# we must constantly update the end position because the drone is moving.
-	end = end_marker.global_position
+	# curve path
 	if t <= 1 and colliding_body != null:
 		var curve_position : Vector2 = bezier_class.quadratic_bezier(start, middle, end, t)
 		colliding_body.position = curve_position
 		t += delta/0.465
 	else:
-		t = 0
-		colliding_body.reparent(self.get_parent())
 		colliding_body.rotation = 0
-		colliding_body.global_position = end_marker.global_position
-		colliding_body.set_deferred("frozen", true)
-		item_picked_up.emit(colliding_body)
+		antidote_collected.emit()
+		# cleanup
 		set_process(false)
+		self.body_entered.disconnect(on_body_entered)
 
-func trigger_pickup():
-	if colliding_body != null:
-		return
-	var overlapping_bodies = self.get_overlapping_bodies()
-	for body in overlapping_bodies:
-		if not body is Antidote:
-			pass
-		self.colliding_body = body
-		self.start = body.global_position
-		var size = self.find_child("CollisionShape2D").shape.size
-		self.middle = self.global_position + Vector2(0, randf_range(-size.y/6, size.y/6))
-		self.set_process(true)
-
-"""
 func on_body_entered(body : RigidBody2D):
 	if !(body is Antidote):
 		return
@@ -56,4 +40,3 @@ func on_body_entered(body : RigidBody2D):
 	var size = self.find_child("CollisionShape2D").shape.size
 	self.middle = self.global_position + Vector2(0, randf_range(-size.y/6, size.y/6))
 	self.set_process(true)
-"""
